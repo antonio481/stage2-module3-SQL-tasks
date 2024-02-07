562cf67a4cbbd9587268809d72d99b22baea0b6e
antonio481 <65397943+antonio481@users.noreply.github.com>
2024-02-07 15:14:40 +0400

    Update SOLUTION.sql
    DecisionCommit

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,29 @@
-
-
+CREATE TABLE Student (
+    id bigint (Primary key),
+    name varchar(255),
+    birthday date(255),
+    group int(255)
+);
+CREATE TABLE Subject (
+    id bigint (Primary key),
+    name varchar(255),
+    description varchar(255),
+    grade int(255)
+);
+CREATE TABLE PaymentType (
+    id bigint (Primary key),
+    name varchar(255),
+);
+CREATE TABLE Payment (
+    id bigint (Primary key),
+    type_id bigint(FOREIGN KEY to PaymentType),
+    amount decimal(255),
+    student_id bigint(FOREIGN KEY to Student),
+    payment_date datetime
+);
+CREATE TABLE Mark (
+    id bigint (Primary key),
+    student_id bigint(FOREIGN KEY to Student),
+    subject_id bigint(FOREIGN KEY to Subject),
+    mark int(255)
+);
