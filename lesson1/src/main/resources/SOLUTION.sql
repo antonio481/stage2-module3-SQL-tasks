19e31005181c403fba3d2cd068549496d816d930
antonio481 <65397943+antonio481@users.noreply.github.com>
2024-02-08 16:23:23 +0400

    Update SOLUTION.sql
    Decision Commit

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,29 +1,46 @@
 CREATE TABLE Student (
-    id bigint (Primary key),
+    id bigint,
     name varchar(255),
     birthday date(255),
-    group int(255)
+    group int(255),
+    PRIMARY KEY (id)
 );
 CREATE TABLE Subject (
-    id bigint (Primary key),
+    id bigint,
     name varchar(255),
     description varchar(255),
-    grade int(255)
+    grade int(255),
+    PRIMARY KEY (id)
 );
 CREATE TABLE PaymentType (
-    id bigint (Primary key),
+    id bigint,
     name varchar(255),
+    PRIMARY KEY (id)
 );
 CREATE TABLE Payment (
-    id bigint (Primary key),
-    type_id bigint(FOREIGN KEY to PaymentType),
+    id bigint,
+    type_id bigint,
     amount decimal(255),
-    student_id bigint(FOREIGN KEY to Student),
-    payment_date datetime
+    student_id bigint,
+    payment_date datetime,
+    CONSTRAINT FK_PaymentType
+    FOREIGN KEY (type_id)
+    REFERENCES PaymentType (id),
+    CONSTRAINT FK_Student
+    FOREIGN KEY (student_id)
+    REFERENCES Student (id),
+    PRIMARY KEY (id)
 );
 CREATE TABLE Mark (
-    id bigint (Primary key),
-    student_id bigint(FOREIGN KEY to Student),
-    subject_id bigint(FOREIGN KEY to Subject),
-    mark int(255)
+    id bigint,
+    student_id bigint,
+    subject_id bigint,
+    mark int(255),
+    CONSTRAINT FK_Student
+    FOREIGN KEY (student_id)
+    REFERENCES Student (id),
+    CONSTRAINT FK_Subject
+    FOREIGN KEY (subject_id)
+    REFERENCES Subject (id),
+    PRIMARY KEY (id)
 );
