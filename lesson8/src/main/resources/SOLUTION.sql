b648f5969db32059a0b0dc7d23fee81bbb7836b9
antonio481 <65397943+antonio481@users.noreply.github.com>
2024-02-28 12:19:28 +0400

    Update SOLUTION.sql
    DecisionCommit

---
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -0,0 +1,4 @@
+Select * FROM Student ORDER by birthday LIMIT 1;
+Select * FROM Payment ORDER by payment_date LIMIT 1;
+Select avg(mark) From Mark where subject_id=4;
+Select min(amount) FROM Payment where type_id=2;
