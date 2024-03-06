0da8685b2c4fd6e54317c7a40bd42e1adcbdf64c
antonio481 <65397943+antonio481@users.noreply.github.com>
2024-03-06 17:45:02 +0400

    Update SOLUTION.sql
    DecisionCommit

---
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -0,0 +1,4 @@
+Update Subject SET grade=5 WHERE name=‘End of Suburbia: Oil Depletion and the Collapse of the American Dream’;
+Update  Student SET group=8 WHERE name=‘Tremaine Worvill’;
+Update Payment SET amount=500, student_id=2 WHERE payment_date > 2021-01-01 and type=2; 
+Update Mark mark=2 WHERE subject_id=315;
