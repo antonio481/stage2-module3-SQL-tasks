d8074092710d24dd614acd1323caff56e5c0030b
antonio481 <65397943+antonio481@users.noreply.github.com>
2024-03-07 16:01:03 +0400

    Update SOLUTION.sql
    FinalDecision

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 Delete Student, Payment, Mark  FROM Student INNER JOIN Payment ON Student.id=Payment.student_id INNER JOIN Mark ON  Student.id=Mark.student_id INNER JOIN Subject ON Subject.id=Mark.subject_id WHERE Subject.grade>5;
 Delete Student FROM Student INNER JOIN Mark ON Student.id=Mark.student_id WHERE Mark.mark<4;
 Delete PaymentType FROM PaymentType INNER JOIN Payment ON PaymentType.id=Payment.type_id WHERE PaymentType.name='Daily';
-DELETE Mark FROM Mark WHERE mark>5;
+DELETE Mark FROM Mark WHERE mark<5;
